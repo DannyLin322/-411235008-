dc577f9e25ebdbc444b1f2e33b239953bc7377d6
DannyLin322 <danny80325@gmail.com>
2026-04-12 21:37:57 +0800

    Add files via upload

---
+++ 113.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> result;
+        vector<int> currentPath;
+        dfs(root, targetSum, currentPath, result);
+        return result;
+    }
+
+private:
+    void dfs(TreeNode* node, int targetSum, vector<int>& path, vector<vector<int>>& result) {
+        if (!node) return;
+
+        path.push_back(node->val);
+
+        if (!node->left && !node->right && node->val == targetSum) {
+            result.push_back(path);
+        } else {
+            dfs(node->left, targetSum - node->val, path, result);
+            dfs(node->right, targetSum - node->val, path, result);
+        }
+
+        path.pop_back();
+    }
+};
